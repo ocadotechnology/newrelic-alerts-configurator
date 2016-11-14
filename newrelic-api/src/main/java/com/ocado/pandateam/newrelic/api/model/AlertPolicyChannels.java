58ed9e0b01a5f58053dc4b9cef4654412b2b36d4
Łukasz Drabik <lukasz.drabik@ocado.com>
2016-11-14 18:35:35 +0100

    Alert policy channels

---
+++ newrelic-api/src/main/java/com/ocado/pandateam/newrelic/api/model/AlertPolicyChannels.java
@@ -0,0 +1,18 @@
+package com.ocado.pandateam.newrelic.api.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Value;
+
+import java.util.Collection;
+
+@Value
+@Builder
+@AllArgsConstructor
+public class AlertPolicyChannels {
+    @JsonProperty("policy_id")
+    Integer policyId;
+    @JsonProperty("channel_ids")
+    Collection<Integer> channelIds;
+}
