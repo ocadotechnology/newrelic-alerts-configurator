0e34114cc4e3230f5fca6f426c51d58701aefce2
Tomasz Styczen <tomasz.styczen@ocado.com>
2016-11-15 17:26:42 +0100

    add missing test

--- newrelic-sync/src/test/java/com/ocado/pandateam/newrelic/sync/SynchronizerTest.java
+++ newrelic-sync/src/test/java/com/ocado/pandateam/newrelic/sync/SynchronizerTest.java
@@ -7,16 +7,25 @@ import com.ocado.pandateam.newrelic.api.AlertsExternalServiceConditionsApi;
 import com.ocado.pandateam.newrelic.api.AlertsPoliciesApi;
 import com.ocado.pandateam.newrelic.api.ApplicationsApi;
 import com.ocado.pandateam.newrelic.api.NewRelicApi;
+import com.ocado.pandateam.newrelic.api.model.applications.Application;
+import com.ocado.pandateam.newrelic.api.model.channels.AlertChannel;
+import com.ocado.pandateam.newrelic.api.model.channels.AlertChannelConfiguration;
 import com.ocado.pandateam.newrelic.sync.channel.Channel;
 import com.ocado.pandateam.newrelic.sync.channel.EmailChannel;
 import com.ocado.pandateam.newrelic.sync.channel.SlackChannel;
 import com.ocado.pandateam.newrelic.sync.configuration.ChannelConfiguration;
 import com.ocado.pandateam.newrelic.sync.configuration.Configuration;
 import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.Optional;
+
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -47,7 +56,8 @@ public class SynchronizerTest {
     private static final float APP_APDEX_THRESHOLD = 1f;
     private static final float USER_APDEX_THRESHOLD = 1f;
 
-    private static final Channel EMAIL_CHANNEL = new EmailChannel("channel1", "email", true);
+    private static final String EMAIL_CHANNEL_NAME = "emailChannel";
+    private static final Channel EMAIL_CHANNEL = new EmailChannel(EMAIL_CHANNEL_NAME, "email", true);
     private static final Channel SLACK_CHANNEL = new SlackChannel("channel2", "slackUrl", "slackName");
 
     @Before
@@ -56,6 +66,25 @@ public class SynchronizerTest {
         mockApi();
         mockValidConfiguration();
         mockValidChannelConfiguration();
+        when(alertsChannelsApiMock.create(any(AlertChannel.class))).thenReturn(createEmailAlertChannel(10, "test"));
+        when(applicationsApiMock.getByName(anyString())).thenReturn(Optional.of(Application.builder().id(1).build()));
+    }
+
+    @Test
+    public void aTest() {
+
+    }
+
+    @Test
+    @Ignore
+    public void shouldRemoveDuplicatedChannels() throws NewRelicSyncException {
+        // given
+        when(alertsChannelsApiMock.list()).thenReturn(ImmutableList.of(createEmailAlertChannel(1, EMAIL_CHANNEL_NAME), createEmailAlertChannel(2, EMAIL_CHANNEL_NAME)));
+        // when
+        testee.sync();
+
+        // then
+
     }
 
     private void mockApi() {
@@ -75,5 +104,11 @@ public class SynchronizerTest {
 
     private void mockValidChannelConfiguration() {
         when(channelConfigurationMock.getChannels()).thenReturn(ImmutableList.of(EMAIL_CHANNEL, SLACK_CHANNEL));
+    }
+
+    private static AlertChannel createEmailAlertChannel(int id, String name) {
+        AlertChannelConfiguration config = AlertChannelConfiguration.builder()
+                .recipients("recipents").includeJsonAttachment("true").build();
+        return AlertChannel.builder().id(id).name(name).type("email").configuration(config).build();
     }
 }
