62ca0fd0d23c452e6978d0cbb916c3ba27ee5cf7
Tomasz Styczen <tomasz.styczen@ocado.com>
2016-11-18 12:06:30 +0100

    fix channels comparision

--- newrelic-sync/src/main/java/com/ocado/pandateam/newrelic/sync/configuration/channel/ChannelUtils.java
+++ newrelic-sync/src/main/java/com/ocado/pandateam/newrelic/sync/configuration/channel/ChannelUtils.java
@@ -17,8 +17,20 @@ public final class ChannelUtils {
     }
 
     public static boolean same(AlertsChannel alertChannel1, AlertsChannel alertChannel2) {
+        AlertsChannelConfiguration alertsChannelConfiguration1 = alertChannel1.getConfiguration();
+        AlertsChannelConfiguration alertsChannelConfiguration2 = alertChannel2.getConfiguration();
         return sameInstance(alertChannel1, alertChannel2)
-            && alertChannel1.getConfiguration().equals(alertChannel2.getConfiguration());
+            && StringUtils.equals(alertsChannelConfiguration1.getUserId(), alertsChannelConfiguration2.getUserId())
+            && StringUtils.equals(alertsChannelConfiguration1.getChannel(), alertsChannelConfiguration2.getChannel())
+            && StringUtils.equals(alertsChannelConfiguration1.getUrl(), alertsChannelConfiguration2.getUrl())
+            && alertsChannelConfiguration1.getIncludeJsonAttachment() == alertsChannelConfiguration2.getIncludeJsonAttachment()
+            && StringUtils.equals(alertsChannelConfiguration1.getRecipients(), alertsChannelConfiguration2.getRecipients())
+            && StringUtils.equals(alertsChannelConfiguration1.getPayloadType(), alertsChannelConfiguration2.getPayloadType())
+            && StringUtils.equals(alertsChannelConfiguration1.getPayload(), alertsChannelConfiguration2.getPayload())
+            && alertsChannelConfiguration1.getHeaders().equals(alertsChannelConfiguration2.getHeaders())
+            && StringUtils.equals(alertsChannelConfiguration1.getBaseUrl(), alertsChannelConfiguration2.getBaseUrl())
+            && StringUtils.equals(alertsChannelConfiguration1.getAuthUsername(), alertsChannelConfiguration2.getAuthUsername())
+            && StringUtils.equals(alertsChannelConfiguration1.getAuthPassword(), alertsChannelConfiguration2.getAuthPassword());
     }
 
     public static AlertsChannelConfiguration generateAlertsChannelConfiguration(Channel channel) {
