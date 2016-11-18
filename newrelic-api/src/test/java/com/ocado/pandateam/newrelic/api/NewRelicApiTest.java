cf72ac7c505000e95f0b0ecf36f6472712e067fb
Łukasz Drabik <lukasz.drabik@ocado.com>
2016-11-18 13:46:51 +0100

    New relic api - post review

--- newrelic-api/src/test/java/com/ocado/pandateam/newrelic/api/NewRelicApiTest.java
+++ newrelic-api/src/test/java/com/ocado/pandateam/newrelic/api/NewRelicApiTest.java
@@ -1,9 +1,9 @@
 package com.ocado.pandateam.newrelic.api;
 
 import com.github.tomakehurst.wiremock.junit.WireMockClassRule;
-import com.google.common.io.Resources;
 import com.ocado.pandateam.newrelic.api.exception.NewRelicApiException;
 import com.ocado.pandateam.newrelic.api.model.applications.Application;
+import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpHeaders;
 import org.junit.Assert;
 import org.junit.Before;
@@ -20,7 +20,6 @@ import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static com.google.common.io.Resources.getResource;
 import static org.apache.http.HttpStatus.SC_OK;
 
 public class NewRelicApiTest {
@@ -40,7 +39,7 @@ public class NewRelicApiTest {
     public void setUp() throws IOException {
         WIRE_MOCK.resetMappings();
         testee = new NewRelicApi("http://localhost:6766", "secret").getApplicationsApi();
-        applications = Resources.toString(getResource("applications.json"), UTF_8);
+        applications = IOUtils.toString(NewRelicApiTest.class.getResource("/applications.json"), UTF_8);
     }
 
     @Test
