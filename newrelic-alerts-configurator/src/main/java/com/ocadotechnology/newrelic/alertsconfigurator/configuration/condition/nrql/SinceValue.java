c730f837b7bc5702aa9984a371b4b89637495f61
marek.soluch <marek.soluch@ocado.com>
2018-09-24 18:38:27 +0200

    Added more values to since enum

--- newrelic-alerts-configurator/src/main/java/com/ocadotechnology/newrelic/alertsconfigurator/configuration/condition/nrql/SinceValue.java
+++ newrelic-alerts-configurator/src/main/java/com/ocadotechnology/newrelic/alertsconfigurator/configuration/condition/nrql/SinceValue.java
@@ -8,7 +8,11 @@ public enum SinceValue {
     SINCE_2_MINUTES("2"),
     SINCE_3_MINUTES("3"),
     SINCE_4_MINUTES("4"),
-    SINCE_5_MINUTES("5");
+    SINCE_5_MINUTES("5"),
+    SINCE_10_MINUTES("10"),
+    SINCE_15_MINUTES("15"),
+    SINCE_30_MINUTES("30"),
+    SINCE_60_MINUTES("60");
 
     String since;
 
